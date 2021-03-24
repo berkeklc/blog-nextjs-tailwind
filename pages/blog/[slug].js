96bdc931d849511edb3ae6fde35c493156107640
berkeklc <berkeklc.123@gmail.com>
2021-03-24 14:37:19 +0300

    design update, date-fns added and localized.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import { blogPosts } from '../../lib/data';
+import {format, parseISO, add} from 'date-fns'
+import { blogPosts } from '../lib/data'
+const turkishLocale = require('date-fns/locale/tr');
 
 export default function BlogPage({ title, date, content}) {
   return (
@@ -10,9 +12,12 @@ export default function BlogPage({ title, date, content}) {
       </Head>
 
       <main>
-        <h1>
+      <div className="border-b-2 border-gray-200 pb-2 mb-4">
+        <h2 className="text-3xl font-bold mb-2">
          {title}
-        </h1>
+        </h2>
+        <div className="text-gray-600 text-xs">{format(parseISO(date), 'MMMM do, uuu', { locale: turkishLocale })}</div>
+      </div>
         <div>{content}</div>
       </main>
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
-
+import {format, parseISO, add} from 'date-fns'
 import { blogPosts } from '../lib/data'
-
+const turkishLocale = require('date-fns/locale/tr');
 export default function Home() {
   return (
     <div>
@@ -11,26 +11,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-         Blog
-        </h1>
-      </main>
 
-      <div>
+
+      <div className="space-y-4">
         {blogPosts.map((item) => (
-          <div key={item.slug}>
-            <div>
-              <Link href={`/blog/${item.slug}`}>
-                <a>{item.title}</a>
-              </Link>
-            </div>
-            <div>{item.date.toString()}</div>
-            <div>{item.content}</div>
-          </div>
+         <BlogListItem key={item.slug} {... item} />
         ))}
       </div>
 
     </div>
+  );
+}
+
+function BlogListItem ({ slug, title, date, content}) {
+  return (
+    <div className="border border-blue-300 shadow hover:shadow-md hover:border-blue-800 rounded-md p-4 transition duration-300 ease-in-out">
+    <div>
+      <Link href={`/blog/${slug}`}>
+        <a className="text-lg font-bold ">{title}</a>
+      </Link>
+    </div>
+    <div className="text-gray-600 text-xs">{format(parseISO(date), 'MMMM do, uuu', { locale: turkishLocale })}</div>
+    <div>{content}</div>
+  </div>
   );
 }
