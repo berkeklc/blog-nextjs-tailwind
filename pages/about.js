3979304163cc9006b27c63a8426d4cdfe2a17c8c
berkeklc <berkeklc.123@gmail.com>
2021-03-24 12:49:14 +0300

    Adding url structure and static data has been created.

--- pages/about.js
+++ pages/about.js
@@ -15,5 +15,5 @@ export default function About() {
       </main>
 
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { blogPosts } from '../lib/data'
 
 export default function Home() {
   return (
@@ -14,6 +17,20 @@ export default function Home() {
         </h1>
       </main>
 
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
+
     </div>
-  )
+  );
 }
