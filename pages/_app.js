6264176b08beeae2308e5f9cb55d85510325e22e
berkeklc <berkeklc.123@gmail.com>
2021-03-24 19:23:44 +0300

    dark mode added

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,47 @@
 import Link from 'next/link'
-
+import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="mx-auto my-8 w-9/12">
+    <div className="mx-auto my-8 w-9/12 ">
     <header>
-      <h1 className="text-6xl font-bold text-center">Berke Kılıç</h1>
+    <Link href="/">
+      <a>
+       <h1 className="text-6xl font-bold text-center">Berke Kılıç</h1>
+      </a>
+      
+    </Link>
       <nav className="my-4">
           <ul className="flex flex-row justify-center space-x-4">
             <li>
               <Link href="/">
                 <a>Home</a> 
-                </Link>
+              </Link>
             </li>
             <li>
               <Link href="/about">
                 <a>About</a> 
               </Link>
             </li>
+            <li>
+           
+            </li>
           </ul>
       </nav>
     </header>
+    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
       <Component {...pageProps} />
+    </ThemeProvider>
+      <footer className="text-center mt-4">
+        <Link href="https://github.com/berkeklc/blog-nextjs-tailwind" >
+          <a target="_blank">
+           GitHub
+          </a>
+        </Link>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default MyApp
